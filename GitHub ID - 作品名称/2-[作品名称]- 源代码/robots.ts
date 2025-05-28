864938a29ec987e07701c94847fce7cab9c101bf
juju <2252376640@qq.com>
2025-05-28 08:44:08 +0800

    Add files via upload

---
+++ GitHub ID - 作品名称/2-[作品名称]- 源代码/robots.ts
@@ -0,0 +1,14 @@
+
+// SEO
+import { MetadataRoute } from 'next'
+ 
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: {
+      userAgent: '*',
+      allow: '/',
+      disallow: '',
+    },
+    sitemap: 'https://www.openloong.org.cn',
+  }
+}
